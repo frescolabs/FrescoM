15b8b5c2f26501259fde4fd703f3b9621af76750
Pavel Katunin <pavelkatunin@pavels-mbp.home>
2021-01-31 10:01:11 +0000

    Class loader fix

--- software/services/fresco_calss_loader.py
+++ software/services/fresco_calss_loader.py
@@ -1,8 +1,9 @@
 
 
 class FrescoClassLoader:
+
+
     def import_class(self, path: str):
-        print('Class path to load')
         dot_components = path.split('.')
         components = dot_components[1].split('/')
         module_string = '.'.join(components)[1:]
@@ -11,5 +12,5 @@ class FrescoClassLoader:
         print('Module name ' + module_string)
         print('Class name ' + class_string)
         mod = __import__(module_string, fromlist=[class_string])
-        klass = getattr(mod, 'AllWellsPhotoProtocol')
+        klass = getattr(mod, class_string)
         return klass
